10268b29fecfe356cff86cf1465088766153b2e3
Dima Dergachov <dergachovda@gmail.com>
2016-04-26 11:29:28 +0300

    add less11 classwork game

---
+++ src/less11/classwork/game/Board.java
@@ -0,0 +1,87 @@
+package less11.classwork.game;
+
+import java.util.Arrays;
+
+public class Board {
+
+    private char[][] board = new char[3][3];
+    private char currentPlayer;
+
+    public Board() {
+        for (int i = 0; i < 3; i++) {
+            Arrays.fill(board[i], ' ');
+        }
+        currentPlayer = 'X';
+    }
+
+    public boolean gameFinished() {
+		char ch = getWinnerPlayer();
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == ch && board[i][1] == ch && board[i][2] == ch)
+                return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == ch && board[1][i] == ch && board[2][i] == ch)
+                return true;
+        }
+
+        if (board[0][0] == ch && board[1][1] == ch && board[2][2] == ch)
+                return true;
+             
+        if (board[2][0] == ch && board[1][1] == ch && board[0][2] == ch)
+                return true;
+
+        return false;
+    }
+
+    public boolean makeMove(String move) {
+        int x = Character.getNumericValue(move.charAt(0)) - 1;
+        int y = Character.getNumericValue(move.charAt(1)) - 1;
+
+        if (!isMoveValid(x, y))
+			return false;
+		
+		board[x][y] = currentPlayer;
+        changeCurrentPlayer();
+        return true;
+    }
+
+    private void changeCurrentPlayer() {
+        if (currentPlayer == 'X')
+            currentPlayer = 'O';
+        else
+            currentPlayer = 'X';
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+	
+	public char getWinnerPlayer() {
+		if (currentPlayer == 'X')
+			return 'O';
+		else
+			return 'X';
+    }
+
+    private boolean isMoveValid(int x, int y) {
+        if (x > 2 || y > 2 || x < 0 || y < 0)
+            return false;
+		if (board[x][y] != ' ')
+			return false;
+        return true;
+    }
+
+    public void printBoard() {
+		System.out.println("-------");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + board[i][j]);
+            }
+            System.out.print("|\n");
+            System.out.println("-------");
+        }
+    }
+
+}
---
+++ src/less11/classwork/game/TicTacToe.java
@@ -0,0 +1,26 @@
+package less11.classwork.Game;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Game started...");
+
+        Board board = new Board();
+
+        while (!board.gameFinished()) {
+            System.out.println("Player " + board.getCurrentPlayer() + " move...");
+            System.out.println("Enter your move: ");
+            String move = scanner.next();
+            if (!board.makeMove(move))
+                System.out.println("Input incorrect/ Repeat your move.");
+            board.printBoard();
+        }
+
+        System.out.println("The winner is " + board.getWinnerPlayer());
+
+    }
+
+}
