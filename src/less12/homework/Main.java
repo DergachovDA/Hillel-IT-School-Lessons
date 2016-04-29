4ccda238ae5c21d84bee8c2b8238068633a935bb
Dima Dergachov <dergachovda@gmail.com>
2016-04-29 14:21:29 +0300

    fixed

--- src/less12/homework/Main.java
+++ src/less12/homework/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Shape[] shapes = new Shape[3];
-        
+
         shapes[0] = new Circle(3);
         shapes[1] = new Square(3);
         shapes[2] = new Rectangle(3, 5);
