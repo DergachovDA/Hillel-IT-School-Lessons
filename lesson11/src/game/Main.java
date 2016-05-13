06e9b165a8efe68b9148dafa64966320809b6398
Dima Dergachov <dergachovda@gmail.com>
2016-05-13 15:07:43 +0300

    renam TicTacToe to Main

--- lesson11/src/game/Main.java
+++ lesson11/src/game/Main.java
@@ -2,7 +2,7 @@ package game;
 
 import java.util.Scanner;
 
-public class TicTacToe {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
