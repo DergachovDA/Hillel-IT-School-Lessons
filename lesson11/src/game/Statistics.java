7e72a74b995a2a3fda19d3fd6bb6e5f6d0df5238
Dima Dergachov <dergachovda@gmail.com>
2016-05-13 11:04:10 +0300

    add classes to less11/game

---
+++ lesson11/src/game/Statistics.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Statistics {
+
+    private List<GameResult> results = new ArrayList<GameResult>();
+
+    public void addResult(GameResult result) {
+        results.add(result);
+    }
+}
